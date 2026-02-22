56cae3cf212dd6f4a79f4f42620e9e60935f2520
vasanth2006-git <vasanthvasanth6016@gmail.com>
2026-02-22 19:29:15 +0530

    Add files via upload
    added day2 python

---
+++ day2.py
@@ -0,0 +1,32 @@
+class Cgpa:
+    def __init__(self):
+        self.cgpa = []
+        self.result = []
+
+    def check_elgiblity(self):
+        for i in self.cgpa:
+            if i <= 70:
+                self.result.append("NA")
+            elif i > 70 and i < 86:
+                self.result.append("3R")
+            elif i > 85 and i < 101 :
+                self.result.append("1R")
+            else:
+                self.result.append("invalid")
+        return self.result
+            
+    def get_input(self):
+        no_of_mark = int(input("enter the number of marks"))
+        for a in range(no_of_mark):
+           mark =int(input("percentage:"))
+           self.cgpa.append(mark)
+        
+    def give_output(self):
+        print(self.result)
+
+print("No of rounds for each percantage of marks:")
+result = Cgpa()
+result.get_input()
+result.check_elgiblity()
+result.give_output()
+
